18bff57f3da5e51f3b61448e9e0d79894b5eec1d
nehayward <nehayward@gmail.com>
2015-02-05 14:24:33 -0800

    firt readme commit

---
+++ Decagon/OCRResultView.swift
@@ -0,0 +1,35 @@
+//
+//  RecievedDataViewController.swift
+//  Decagon
+//
+//  Created by Nick Hayward on 11/15/14.
+//  Copyright (c) 2014 nehayward. All rights reserved.
+//
+
+import UIKit
+
+class RecievedDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
